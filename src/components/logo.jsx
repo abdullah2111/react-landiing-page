be45e4c504dc281a381d85a6657db652d4d913cb
Abdullah Al Mamun <abdullah.official2111@gmail.com>
2025-09-19 11:13:44 +0600

    image path fixed

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,12 +1,9 @@
+import logo from "../assets/logo.svg";
 export default function Logo() {
   return (
     <>
       <a href="#" class="flex items-center">
-        <img
-          src="./src/assets/logo.svg"
-          class="h-6 mr-3 sm:h-9"
-          alt="Landwind Logo"
-        />
+        <img src={logo} class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
         <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
           LOGO
         </span>
